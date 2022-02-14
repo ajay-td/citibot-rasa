3cc8d1ab76aaf978400b9a1221e85d9ac55b7279
bavalpreet singh <bavalpreet.singh25@gmail.com>
2022-02-14 16:34:40 +0530

    custom fallback policy

--- actions/actions.py
+++ actions/actions.py
@@ -7,11 +7,11 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
+from typing import Any, Text, Dict, List
+import json
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
 #
 # class ActionHelloWorld(Action):
 #
@@ -25,3 +25,29 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+class ActionFallback(Action):
+    def name(self) -> Text:
+        return "action_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # to get intent of user message
+        _intent=tracker.latest_message['intent'].get('name')
+        print("Intent of user message predicted by Rasa ",_intent)
+
+        print(tracker.latest_message['text']) # to get user typed message 
+
+        intent_found = json.dumps(tracker.latest_message['response_selector'][_intent]['ranking'][0]['intent_response_key'], indent=4)
+        print("retrieval we found (i.e intent response key ) ",intent_found)
+
+        # confidence of retrieval intent we found
+        retrieval_intent_confidence = tracker.latest_message['response_selector'][_intent]['response']['confidence']*100
+        
+        print(f"retrieval_intent_confidence we found was {retrieval_intent_confidence}")
+        if retrieval_intent_confidence<90:
+            dispatcher.utter_message(text=" Sorry can you please rephrase?")
+        else:
+             dispatcher.utter_message(response = intent_found) # use response for defining intent name
+        return []
